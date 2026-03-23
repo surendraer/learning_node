a3062793f34abd5674b87721f1985011b90d2bf9
Surendra Singh <surendra.23it667@rtu.ac.in>
2026-03-23 15:25:50 +0530

    Done

--- auth.js
+++ auth.js
@@ -1,26 +1,29 @@
+// authorisation middleware
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 
-
-
-passport.use(new LocalStrategy(async (username,password,done)=>{
+// middleware to authenticate user
+passport.use(new LocalStrategy(async (username, password, done) => {
   //authentication logic
 
   try {
-    const response = await person.findOne({username : username});
-    if(!response){
-      done(null, false , {message : "user not found"});
+    //finding username in database
+    const response = await person.findOne({ username: username });
+    //if user doent exist
+    if (!response) {
+      done(null, false, { message: "user not found" });
     }
+    //if user exist then comparing its password with the password given in body
     const pass = await response.comparePassword(password);
-
-    if(pass){
-      done(null,response);
-    }else{
-      done(null,false,{message:"wrong password"});
+    // if password id correct
+    if (pass) {
+      done(null, response);
+    } else {
+      done(null, false, { message: "wrong password" });
     }
 
+    // done is a callback function that take differnet argument based on the success or failure shown as above
 
-    
   } catch (error) {
     done(error);
   }
