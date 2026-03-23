320427c4ac42b3a2c7742809520e67665abdc7b2
Surendra Singh <surendra.23it667@rtu.ac.in>
2026-03-23 15:43:11 +0530

    Done

--- routes/menuRoutes.js
+++ routes/menuRoutes.js
@@ -1,39 +1,42 @@
 const express = require("express")
 const router = express.Router();
+// requiring menu model
 const menulist = require("../models/menulist")
 
-router.get("/",async (req,res)=>{
+// get all data
+router.get("/", async (req, res) => {
 
   try {
 
     const data = await menulist.find();
     console.log("menu fetched");
     res.status(200).json(data);
-    
+
   } catch (error) {
 
     console.log("error in fetching");
-    res.status(500).json({error : "error"});
-    
+    res.status(500).json({ error: "error" });
+
   }
 })
 
 
-
-router.post("/",async (req,res) => {
+// posting in menu
+router.post("/", async (req, res) => {
   try {
-
+    // extracting data from body
     const data = req.body;
+    //creating new object
     const newMenu = new menulist(data);
-
+    //adding it to db
     const response = await newMenu.save();
     console.log("added in menu");
     res.status(200).json(data);
-    
+
   } catch (error) {
     console.log("error in saving");
-    res.status(500).json({error:"error"});
-    
+    res.status(500).json({ error: "error" });
+
   }
 })
 
