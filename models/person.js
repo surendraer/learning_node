2e10803bc8e0e07d93d9ebbd3c9c90a09621c17f
Surendra Singh <surendra.23it667@rtu.ac.in>
2026-03-23 15:48:26 +0530

    Done

--- models/person.js
+++ models/person.js
@@ -1,43 +1,46 @@
 const mongoose = require("mongoose");
+// this is used to encrypt the password
 const bcrypt = require("bcrypt");
 
 const personSchema = new mongoose.Schema({
-    name : {
-        type : String,
-        required : true
-    },
-    age : {
-        type : Number,
-        required : true
-    },
-    work :{
+    name: {
         type: String,
-        required : true,
-        enum:["chef","waiter","owner"]
-    },
-    email:{
-        type: String,
-        required:true,
-        unique:true
-    },
-    username : {
-        type: String,
-        required : true
-    },
-    password : {
-        type : String,
         required: true
-    } 
+    },
+    age: {
+        type: Number,
+        required: true
+    },
+    work: {
+        type: String,
+        required: true,
+        enum: ["chef", "waiter", "owner"]
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
 })
 
-personSchema.pre('save', async (next)=>{
+//.pre means we need to do it before saving data in db
+personSchema.pre('save', async (next) => {
     const person = this;
-    if(!person.isModified('password')){
-       return next();
+    // if password is not modified
+    if (!person.isModified('password')) {
+        return next();
     }
     //hash the password only when updated or new
     try {
-// salt genration 
+        // salt genration 
         const salt = await bcrypt.genSalt(10);
         //hash password
         const hashedpassword = await bcrypt.hash(person.password, salt);
@@ -48,21 +51,23 @@ personSchema.pre('save', async (next)=>{
     }
 })
 
-personSchema.methods.comparePassword = async (candidatePassword)=>{
+
+// this is a method which is used for verifying whetehr the entered password is correcct or not 
+personSchema.methods.comparePassword = async (candidatePassword) => {
     try {
 
         const isMatch = await bcrypt.compare(candidatePassword, this.password);
         return isMatch;
-        
+
     } catch (error) {
         throw error;
-        
+
     }
 }
 
 
 // create model
 
-const person = mongoose.model('person',personSchema);
+const person = mongoose.model('person', personSchema);
 
 module.exports = person;
