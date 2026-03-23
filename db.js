2fc3c1cf4e1a96b9b96588a219ee2471958864a5
Surendra Singh <surendra.23it667@rtu.ac.in>
2026-03-23 15:21:09 +0530

    Done

--- db.js
+++ db.js
@@ -1,59 +1,30 @@
-// const mongoose = require('mongoose');
-
-// const mongoURL = 'mongodb://localhost:27017/hotels'//define mongodb url
-
-// //setup mongodb connection
-
-// mongoose.connect(mongoURL)
-
-// const db = mongoose.connection;
-
-// db.on('connected',()=>{
-//     console.log("MongoDB is connected");
-// });
-
-// db.on('error',(err)=>{
-//     console.log("error in connecting db: "+err);
-// });
-
-// db.on('disconnected',()=>{
-//     console.log("database is disconnected");
-// });
-
-
-//export database:
-
-// module.exports = db;
-//comment added for testing
-
+//this file is used to create the connection with database
 
 // import mongoose 
 const mongoose = require("mongoose");
 require("dotenv").config();
 // import the url 
-
 const MongoURL = 'mongodb://localhost:27017/hotels';
+//Online database url with env security
 //const MongoURL = process.env.DB_URL;
-// create connection 
 
+// create connection 
 mongoose.connect(MongoURL);
 
 // get the obj
-
 const db = mongoose.connection;
 
 // set event listeners
-
-db.on("connected",()=>{
+db.on("connected", () => {
     console.log("database connected");
 });
 
-db.on("disconnected",()=>{
+db.on("disconnected", () => {
     console.log("database disconnected");
 });
 
-db.on("error",(err)=>{
-    console.log("error in connecting: "+err);
+db.on("error", (err) => {
+    console.log("error in connecting: " + err);
 });
 
 module.exports = db;
