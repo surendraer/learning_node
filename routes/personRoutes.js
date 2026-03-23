278271a5e39c2841d08e3e1d762fa88f0df83bd1
Surendra Singh <surendra.23it667@rtu.ac.in>
2026-03-23 15:41:17 +0530

    Done

--- routes/personRoutes.js
+++ routes/personRoutes.js
@@ -1,19 +1,22 @@
 const express = require("express")
 const router = express.Router();
+// need persons model to create entry in db
 const person = require("../models/person")
-const {jasonmiddleware,jwttokengenerator} = require("../jwt");
-
+const { jasonmiddleware, jwttokengenerator } = require("../jwt");
+//signup route
 router.post("/signup", async (req, res) => {
 
     try {
         const data = req.body;
-
+        // creating a new object of person using the data
         const newPerson = new person(data);
+        // adding that entry in the database
         const response = await newPerson.save();
-
+        // after adding it now we are creating a token
         const token = jwttokengenerator(response.email);
         console.log("data saved and token is ", token);
-        res.status(200).json({response : response, token: token});
+        // sending the token to the browser
+        res.status(200).json({ response: response, token: token });
 
     } catch (error) {
         console.log(error);
@@ -26,29 +29,30 @@ router.post("/signup", async (req, res) => {
 })
 
 // login route
-router.post("/login", async (req,res)=>{
+router.post("/login", async (req, res) => {
+    // extracting the data 
+    const { username, password } = req.body;
+    // finding the username
+    const user = await person.findOne({ username: username });
 
-    const {username,password} = req.body;
-
-    const user = await person.findOne({username:username});
-
-    if(!user || !(await user.comparePassword(password))){
-        return res.status(500).json({error: "invalid user"});
+    if (!user || !(await user.comparePassword(password))) {
+        return res.status(500).json({ error: "invalid user" });
     }
-
+    //if user exist we generate the token
     try {
 
         const token = jwttokengenerator(req.body.email);
         res.json(token);
-        
+
     } catch (error) {
 
-        res.status(500).json({error: "internal server error"});
-        
+        res.status(500).json({ error: "internal server error" });
+
     }
 
 })
 
+//fetch all data
 router.get("/", async (req, res) => {
     try {
 
@@ -63,7 +67,7 @@ router.get("/", async (req, res) => {
     }
 })
 
-
+// fetch data based on some filter
 router.get("/:work", async (req, res) => {
     try {
         const worktype = req.params.work;
@@ -81,12 +85,14 @@ router.get("/:work", async (req, res) => {
     }
 })
 
+// fetching based on id
 router.put("/:id", async (req, res) => {
     try {
         const personid = req.params.id;
         const persondata = req.body;
 
         const response = await person.findByIdAndUpdate(personid, persondata, {
+            // this will make sure the response contasins latest details
             new: true,
             runValidators: true
         })
@@ -108,9 +114,12 @@ router.put("/:id", async (req, res) => {
 
 
 //profile route
-router.get("/person", jasonmiddleware, async (req,res)=>{
+router.get("/person", jasonmiddleware, async (req, res) => {
+    // this is given by the niddleware
     const userData = req.user;
+    // getting id 
     const userid = userData.id;
+    // giving data of that id
     const user = await person.findById(userid);
 
     res.status(200).json(user);
